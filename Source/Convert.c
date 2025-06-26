dfd7892c070732b3315f4247a87620d7d5379ef9
kynex7510 <nikkitten3@gmail.com>
2025-06-26 23:25:55 +0200

    Revert "Remove cache invalidate function"
    This reverts commit 342f3a33a67611a825863511fe97c1ea7abad239.

--- Source/Convert.c
+++ Source/Convert.c
@@ -29,6 +29,7 @@ bool ripConvertFromNative(const u8* src, u8* dst, u16 width, u16 height, RIPPixe
 
     bool ret = false;
     if (ripTile(src, tmp, width, height, pixelFormat)) {
+        ripInvalidateDataCache(tmp, size);
         ripSwapBytes(tmp, dst, width, height, pixelFormat, flip);
         ret = true;
     }
@@ -48,6 +49,7 @@ bool ripConvertInPlaceToNative(u8* p, u16 width, u16 height, RIPPixelFormat pixe
 
     bool ret = false;
     if (ripTile(p, tmp, width, height, pixelFormat)) {
+        ripInvalidateDataCache(tmp, size);
         memcpy(p, tmp, size);
         ret = true;
     }
@@ -64,6 +66,7 @@ bool ripConvertInPlaceFromNative(u8* p, u16 width, u16 height, RIPPixelFormat pi
 
     bool ret = false;
     if (ripUntile(p, tmp, width, height, pixelFormat)) {
+        ripInvalidateDataCache(tmp, size);
         memcpy(p, tmp, size);
         ripSwapBytesInPlace(p, width, height, pixelFormat);
         ret = true;
